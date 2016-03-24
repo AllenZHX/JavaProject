61de6d392af200b683c8448d3ff32cb1994ff21a
AllenZHX <hzheng9@stevens.edu>
2016-03-24 13:42:37 -0400

    draw the interface_005(almost done)

--- JavaProject/src/JPaneTest.java
+++ JavaProject/src/JPaneTest.java
@@ -1,7 +1,11 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.text.SimpleDateFormat;  
+import java.util.Calendar;  
+import java.util.Date;  
+import java.util.Timer;  
+import java.util.TimerTask;  
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
@@ -10,21 +14,37 @@ public class JPaneTest extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	JButton[] button1 = new JButton[2];
 	JButton[] button2 = new JButton[5];
+	JButton[] button5 = new JButton[4];
+	JButton[] button6 = new JButton[3];
 	JLabel[] label1 = new JLabel[3];
 	JLabel[] label2 = new JLabel[4];
+	JLabel[] label5 = new JLabel[5];
+	JLabel[] label6 = new JLabel[5];
 	JTextArea[] ta1 = new JTextArea[2];
+	JTextArea[] ta5 = new JTextArea[4];
+	JTextArea[] ta6 = new JTextArea[4];
 	JTable table = new JTable();
 	ScrollPane scrollPane = new ScrollPane();
 	String[] buttonString1_6 = {"OK", "Canel"};
 	String[] buttonString2 = {"Family Room","Double Room","Single Room","OK", "Canel"};
 	String[] labelString1 = {"Check-in", "Name:","ID:"};
 	String[] labelString2 = {"Booking"};
+	String[] labelString5 = {"Check-out","Room #: ","Total: ","Paid-up: ","Change: "};
+	String[] labelString6 = {"ORDER","Room #: ","Item: ","Num: ","Total: "};
 	String[] buttonString22 = {"F1","F2","D1","D2","D3","D4","S1","S2","S3",
 			                  "F3","F4","D5","D6","D7","D8","S4","S5","S6",
 			                  "F5","F6","D9","D10","D11","D12","S7","S8","S9"};
+	String[] buttonString5 = {"Settle","confirm","Finish","Canel"};
+	String[] buttonString6 = {"Settle","OK","Canel"};
 	Font font =  new Font("Times new Roman", Font.BOLD, 20);
 	Font font1 =  new Font("Times new Roman", Font.BOLD, 34);
 	Font font2 =  new Font("Times new Roman", Font.BOLD, 14);
+	
+	JLabel timeLabel;  
+    JLabel displayArea;  
+    String DEFAULT_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";  
+    String time;  
+    int ONE_SECOND = 1000;
 	
 	public JPaneTest() {
 		setTitle("Hotel management System");
@@ -111,19 +131,92 @@ public class JPaneTest extends JFrame implements ActionListener{
 		p3.add(scrollPane);
 		
 		// ******for Panel4(standard time Part)******
-		p4.add(new JButton("1"));
-		p4.add(new JButton("2"));
-		p4.add(new JButton("3"));
+		
+		timeLabel = new JLabel("CurrentTime: ");  
+        displayArea = new JLabel();  
+        timeLabel.setFont(font);
+        displayArea.setFont(font);
+        p4.setLayout(null);
+        timeLabel.setBounds(120,100,140,50);
+        displayArea.setBounds(100,200,180,50);
+        configTimeArea();
+		p4.add(timeLabel);
+		p4.add(displayArea);
 		
 		// ******for Panel5(Check-out Part)******
-		p5.add(new JButton("1"));
-		p5.add(new JButton("2"));
-		p5.add(new JButton("3"));
-		p5.add(new JButton("4"));
+		for(int i = 0; i < 4; i++) {
+		    button5[i] = new JButton();
+		    button5[i].setText(buttonString5[i]);
+		    button5[i].setFont(font2);
+		}
+		for(int i = 0; i < 5; i++) {
+		    label5[i] = new JLabel();
+		    label5[i].setText(labelString5[i]);
+		    label5[i].setFont(font);
+		    if (i==0)
+		    	label5[i].setFont(font1);
+		}
+		for(int i = 0; i < 4; i++) {
+		    ta5[i] = new JTextArea();
+		    ta5[i].setFont(font);
+		}
+		p5.setLayout(null);
+		button5[0].setBounds(260,105,80,40);
+		button5[1].setBounds(250,215,100,40);
+		button5[2].setBounds(40,310,100,40);
+		button5[3].setBounds(240,310,100,40);
+		label5[0].setBounds(120,20,160,50);
+		label5[1].setBounds(40,80,80,50);
+		label5[2].setBounds(40,120,80,50);
+		label5[3].setBounds(40,190,80,50);
+		label5[4].setBounds(40,230,80,50);
+		ta5[0].setBounds(140,90,100,30);
+		ta5[1].setBounds(140,200,100,30);
+		ta5[2].setBounds(140,130,100,30);
+		ta5[3].setBounds(140,240,100,30);
+		for(int i = 0; i < 4; i++)
+			p5.add(button5[i]);
+		for(int i = 0; i < 5; i++) 
+			p5.add(label5[i]);
+		for(int i = 0; i < 4; i++) 
+			p5.add(ta5[i]);
 		
-		// ******for Panel3(Order service Part)******
-		p6.add(new JButton("1"));
-		p6.add(new JButton("2"));
+		// ******for Panel6(Order service Part)******
+		for(int i = 0; i < 3; i++) {
+		    button6[i] = new JButton();
+		    button6[i].setText(buttonString6[i]);
+		    button6[i].setFont(font2);
+		}
+		for(int i = 0; i < 5; i++) {
+		    label6[i] = new JLabel();
+		    label6[i].setText(labelString6[i]);
+		    label6[i].setFont(font);
+		    if (i==0)
+		    	label6[i].setFont(font1);
+		}
+		for(int i = 0; i < 4; i++) {
+		    ta6[i] = new JTextArea();
+		    ta6[i].setFont(font);
+		}
+		p6.setLayout(null);
+		button6[0].setBounds(250,155,100,40);
+		button6[1].setBounds(40,310,100,40);
+		button6[2].setBounds(240,310,100,40);
+		label6[0].setBounds(20,20,160,50);
+		label6[1].setBounds(50,80,80,50);
+		label6[2].setBounds(50,130,80,50);
+		label6[3].setBounds(50,170,80,50);
+		label6[4].setBounds(50,230,80,50);
+		ta6[0].setBounds(140,90,100,30);
+		ta6[1].setBounds(140,140,100,30);
+		ta6[2].setBounds(140,180,100,30);
+		ta6[3].setBounds(140,240,100,30);
+		for(int i = 0; i < 3; i++)
+			p6.add(button6[i]);
+		for(int i = 0; i < 5; i++) 
+			p6.add(label6[i]);
+		for(int i = 0; i < 4; i++) 
+			p6.add(ta6[i]);
 		
 		// add all of Panel to Container c
 		c.add(p1);
@@ -135,7 +228,7 @@ public class JPaneTest extends JFrame implements ActionListener{
 		setSize(1200,800);
 		setVisible(true);
 		setResizable(false);
-		setLocation(350,150);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	
@@ -150,7 +243,23 @@ public class JPaneTest extends JFrame implements ActionListener{
             Popup3 hw3 = new Popup3(this);}
             
 	}
-		
+	
+	public void configTimeArea() {  
+        Timer tmr = new Timer();  
+        tmr.scheduleAtFixedRate(new JLabelTimerTask(), new Date(), ONE_SECOND);  
+    }  
+  
+    public class JLabelTimerTask extends TimerTask {  
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(  
+                DEFAULT_TIME_FORMAT);  
+  
+        @Override  
+        public void run() {  
+            time = dateFormatter.format(Calendar.getInstance().getTime());  
+            displayArea.setText(time);  
+        }  
+    } 
+	
 	class Popup implements ActionListener{
 	        JButton[] button22 = new JButton[27];
 	        JDialog jDialog=null; 
