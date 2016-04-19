7494db84fd3692143a13d99b654ae53ca847cd72
Xiang Cao <xcao4@stevens.edu>
2016-04-18 22:38:48 -0400

    Update MyClient.java

--- StudentManegementSystem/src/MyClient.java
+++ StudentManegementSystem/src/MyClient.java
@@ -1,55 +1,32 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.PrintWriter;
-import java.net.Socket;
-
-import javax.swing.*;
-
-
-
-public class MyClient extends JFrame {
-	PrintWriter writer;
-	Container c;
-	Socket socket;
-	JTextArea ta=new JTextArea();
-	JTextField tf=new JTextField();
-	
-	
-	public MyClient(String title){
-		super(title);
-		c=getContentPane();
-		JScrollPane jsp=new JScrollPane();
-		c.add(jsp, BorderLayout.CENTER);
-		jsp.setViewportView(ta);
-		c.add(tf, BorderLayout.SOUTH);
-		
-		tf.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				writer.println(tf.getText());
-				ta.append(tf.getText()+"\n");
-				ta.setSelectionEnd(ta.getText().length());
-				tf.setText("");
-			}
-		});
-		
-		
-		setVisible(true);
-		setSize(800,600);
-		
-	}
-	
-	public void connect() throws Exception{
-		ta.append("try to connect!\n");
-		socket =new Socket("127.0.0.1",9008);
-		writer =new PrintWriter(socket.getOutputStream(),true);
-		ta.append("Finish Connectting!\n");
-	}
-	
-	
-	public static void main(String[] args) throws Exception{
-		MyClient client=new MyClient("send data to server");
-		client.connect();
-		
-	}
-}
+import java.io.*;  
+import java.net.*;  
+/**  
+ * @author hnyer  
+ *  
+ */ 
+public class Client {  
+    public static void main(String[] args) throws Exception, IOException {  
+        Socket client=new Socket("127.0.0.1",9000);//访问其他电脑时,ip地址改成相应的。  
+        System.out.println("连接服务端成功！");  
+        System.out.println("请在控制台输入信息(bye表示结束对话)：");  
+        BufferedReader br1;  
+        BufferedReader br2=new BufferedReader(new InputStreamReader(client.getInputStream()));//从服务端获得输入流  
+        BufferedWriter bw1=new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));//发给服务器  
+        String line=null;  
+        String line2=null;  
+        while(true){  
+            br1=new BufferedReader(new InputStreamReader(System.in));//控制台输入流  
+            line=br1.readLine();  
+            if(line==null){break;}  
+            bw1.write(line);  
+            bw1.newLine();  
+            bw1.flush();  
+            if("bye".equalsIgnoreCase(line)){  
+                System.out.println("对话结束！谢谢使用");  
+                break;  
+            }  
+            line2=br2.readLine();//接收服务器的反馈  
+            System.out.println("服务器说："+line2);//显示服务器的消息  
+        }//while  
+    }  
+}  
